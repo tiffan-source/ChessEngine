8b5c5dc80be7e6afd36f1fbd70f6f17a0c3d0ff5
tiffan-source <zohoungbogboanlyou@gmail.com>
2026-01-04 13:35:33 +0100

    feat: ajouter des tests pour la creation de la cle zobrish a l'initialisation d'une game

--- test/test_game.c
+++ test/test_game.c
@@ -1,14 +1,15 @@
 
-#ifdef TEST
 
 #include "unity.h"
 
 #include "game.h"
 #include "board.h"
 #include "binary_tools.h"
+#include "zobrist_key.h"
 
 void setUp(void)
 {
+    initialize_magic_zobrist_numbers();
 }
 
 void tearDown(void)
@@ -326,4 +327,248 @@ void test_game_should_create_game_from_FEN_with_no_en_passant_target_square(void
     free_game(game);
 }
 
-#endif // TEST
+
+void test_transposition_should_return_valid_zobrist_key_for_two_king_on_e1_and_e8_white_turn_and_no_castling_rights(void)
+{
+    Game* game = create_game_from_FEN("4k3/8/8/8/8/8/8/4K3 w - - 0 1");
+
+    U64 zobrist_key = create_zobrist_key_from_game_state(game);
+
+    U64 expected_key = 0;
+    expected_key ^= king_zobrist_magic_number[1][E1];   // White king on e1
+    expected_key ^= king_zobrist_magic_number[0][E8];  // Black king on e8
+    expected_key ^= castling_rights_zobrist_magic_number[0]; // No castling rights
+
+    TEST_ASSERT_EQUAL_UINT64(expected_key, zobrist_key);
+}
+
+void test_transposition_should_return_valid_zobrist_key_for_starting_position(void)
+{
+    Game* game = create_game_from_FEN("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1");
+
+    U64 zobrist_key = create_zobrist_key_from_game_state(game);
+
+    U64 expected_key = 0;
+    // White Pieces
+    expected_key ^= rook_zobrist_magic_number[1][A1];
+    expected_key ^= knight_zobrist_magic_number[1][B1];
+    expected_key ^= bishop_zobrist_magic_number[1][C1];
+    expected_key ^= queen_zobrist_magic_number[1][D1];
+    expected_key ^= king_zobrist_magic_number[1][E1];
+    expected_key ^= bishop_zobrist_magic_number[1][F1];
+    expected_key ^= knight_zobrist_magic_number[1][G1];
+    expected_key ^= rook_zobrist_magic_number[1][H1];
+    for (int i = A2; i <= H2; i++) expected_key ^= pawn_zobrist_magic_number[1][i];
+
+    // Black Pieces
+    expected_key ^= rook_zobrist_magic_number[0][A8];
+    expected_key ^= knight_zobrist_magic_number[0][B8];
+    expected_key ^= bishop_zobrist_magic_number[0][C8];
+    expected_key ^= queen_zobrist_magic_number[0][D8];
+    expected_key ^= king_zobrist_magic_number[0][E8];
+    expected_key ^= bishop_zobrist_magic_number[0][F8];
+    expected_key ^= knight_zobrist_magic_number[0][G8];
+    expected_key ^= rook_zobrist_magic_number[0][H8];
+    for (int i = A7; i <= H7; i++) expected_key ^= pawn_zobrist_magic_number[0][i];
+
+    // Castling Rights (KQkq = 15)
+    expected_key ^= castling_rights_zobrist_magic_number[15];
+
+    TEST_ASSERT_EQUAL_UINT64(expected_key, zobrist_key);
+}
+
+void test_transposition_should_include_black_to_move(void)
+{
+    Game* game = create_game_from_FEN("rnbqkbnr/pppppppp/8/8/4P3/8/PPPP1PPP/RNBQKBNR b KQkq e3 0 1");
+
+    U64 zobrist_key = create_zobrist_key_from_game_state(game);
+
+    U64 expected_key = 0;
+    // White Pieces (Pawn on E4 instead of E2)
+    expected_key ^= rook_zobrist_magic_number[1][A1];
+    expected_key ^= knight_zobrist_magic_number[1][B1];
+    expected_key ^= bishop_zobrist_magic_number[1][C1];
+    expected_key ^= queen_zobrist_magic_number[1][D1];
+    expected_key ^= king_zobrist_magic_number[1][E1];
+    expected_key ^= bishop_zobrist_magic_number[1][F1];
+    expected_key ^= knight_zobrist_magic_number[1][G1];
+    expected_key ^= rook_zobrist_magic_number[1][H1];
+    for (int i = A2; i <= H2; i++) {
+        if (i != E2) expected_key ^= pawn_zobrist_magic_number[1][i];
+    }
+    expected_key ^= pawn_zobrist_magic_number[1][E4];
+
+    // Black Pieces
+    expected_key ^= rook_zobrist_magic_number[0][A8];
+    expected_key ^= knight_zobrist_magic_number[0][B8];
+    expected_key ^= bishop_zobrist_magic_number[0][C8];
+    expected_key ^= queen_zobrist_magic_number[0][D8];
+    expected_key ^= king_zobrist_magic_number[0][E8];
+    expected_key ^= bishop_zobrist_magic_number[0][F8];
+    expected_key ^= knight_zobrist_magic_number[0][G8];
+    expected_key ^= rook_zobrist_magic_number[0][H8];
+    for (int i = A7; i <= H7; i++) expected_key ^= pawn_zobrist_magic_number[0][i];
+
+    // Black to move
+    expected_key ^= black_to_move_zobrist_magic_number;
+
+    // Castling Rights (KQkq = 15)
+    expected_key ^= castling_rights_zobrist_magic_number[15];
+
+    // En Passant (e3)
+    expected_key ^= en_passant_square_zobrist_magic_number[E3];
+
+    TEST_ASSERT_EQUAL_UINT64(expected_key, zobrist_key);
+}
+
+void test_transposition_should_handle_castling_rights_changes(void)
+{
+    // White King Side (K) and Black Queen Side (q) -> 8 + 1 = 9
+    Game* game = create_game_from_FEN("r3k2r/8/8/8/8/8/8/R3K2R w Kq - 0 1");
+
+    U64 zobrist_key = create_zobrist_key_from_game_state(game);
+
+    U64 expected_key = 0;
+    expected_key ^= rook_zobrist_magic_number[1][A1];
+    expected_key ^= king_zobrist_magic_number[1][E1];
+    expected_key ^= rook_zobrist_magic_number[1][H1];
+    
+    expected_key ^= rook_zobrist_magic_number[0][A8];
+    expected_key ^= king_zobrist_magic_number[0][E8];
+    expected_key ^= rook_zobrist_magic_number[0][H8];
+
+    expected_key ^= castling_rights_zobrist_magic_number[9];
+
+    TEST_ASSERT_EQUAL_UINT64(expected_key, zobrist_key);
+}
+
+void test_transposition_should_handle_mixed_castling_rights_Qk(void)
+{
+    // White Queen Side (Q) and Black King Side (k) -> 4 + 2 = 6
+    Game* game = create_game_from_FEN("r3k2r/8/8/8/8/8/8/R3K2R w Qk - 0 1");
+
+    U64 zobrist_key = create_zobrist_key_from_game_state(game);
+
+    U64 expected_key = 0;
+    expected_key ^= rook_zobrist_magic_number[1][A1];
+    expected_key ^= king_zobrist_magic_number[1][E1];
+    expected_key ^= rook_zobrist_magic_number[1][H1];
+    
+    expected_key ^= rook_zobrist_magic_number[0][A8];
+    expected_key ^= king_zobrist_magic_number[0][E8];
+    expected_key ^= rook_zobrist_magic_number[0][H8];
+
+    expected_key ^= castling_rights_zobrist_magic_number[6];
+
+    TEST_ASSERT_EQUAL_UINT64(expected_key, zobrist_key);
+}
+
+void test_transposition_should_handle_en_passant_target_a3(void)
+{
+    // 1. a4 (White plays a4, en passant target a3)
+    Game* game = create_game_from_FEN("rnbqkbnr/pppppppp/8/8/P7/8/1PPPPPPP/RNBQKBNR b KQkq a3 0 1");
+
+    U64 zobrist_key = create_zobrist_key_from_game_state(game);
+
+    U64 expected_key = 0;
+    
+    // White Pieces (Pawn on A4 instead of A2)
+    expected_key ^= rook_zobrist_magic_number[1][A1];
+    expected_key ^= knight_zobrist_magic_number[1][B1];
+    expected_key ^= bishop_zobrist_magic_number[1][C1];
+    expected_key ^= queen_zobrist_magic_number[1][D1];
+    expected_key ^= king_zobrist_magic_number[1][E1];
+    expected_key ^= bishop_zobrist_magic_number[1][F1];
+    expected_key ^= knight_zobrist_magic_number[1][G1];
+    expected_key ^= rook_zobrist_magic_number[1][H1];
+    
+    expected_key ^= pawn_zobrist_magic_number[1][A4];
+    for (int i = B2; i <= H2; i++) expected_key ^= pawn_zobrist_magic_number[1][i];
+
+    // Black Pieces (Standard)
+    expected_key ^= rook_zobrist_magic_number[0][A8];
+    expected_key ^= knight_zobrist_magic_number[0][B8];
+    expected_key ^= bishop_zobrist_magic_number[0][C8];
+    expected_key ^= queen_zobrist_magic_number[0][D8];
+    expected_key ^= king_zobrist_magic_number[0][E8];
+    expected_key ^= bishop_zobrist_magic_number[0][F8];
+    expected_key ^= knight_zobrist_magic_number[0][G8];
+    expected_key ^= rook_zobrist_magic_number[0][H8];
+    for (int i = A7; i <= H7; i++) expected_key ^= pawn_zobrist_magic_number[0][i];
+
+    // Black to move
+    expected_key ^= black_to_move_zobrist_magic_number;
+
+    // Castling Rights (KQkq = 15)
+    expected_key ^= castling_rights_zobrist_magic_number[15];
+
+    // En Passant (a3)
+    expected_key ^= en_passant_square_zobrist_magic_number[A3];
+
+    TEST_ASSERT_EQUAL_UINT64(expected_key, zobrist_key);
+}
+
+void test_transposition_should_handle_complex_sicilian_position(void)
+{
+    // Sicilian Dragon: "r1bq1rk1/pp2ppbp/2np1np1/8/3NP3/2N1BP2/PPPQ2PP/R3KB1R w KQ - 3 9"
+    Game* game = create_game_from_FEN("r1bq1rk1/pp2ppbp/2np1np1/8/3NP3/2N1BP2/PPPQ2PP/R3KB1R w KQ - 3 9");
+
+    U64 zobrist_key = create_zobrist_key_from_game_state(game);
+
+    U64 expected_key = 0;
+
+    // White Pieces
+    // Rooks: A1, H1
+    expected_key ^= rook_zobrist_magic_number[1][A1];
+    expected_key ^= rook_zobrist_magic_number[1][H1];
+    // Knights: C3, D4
+    expected_key ^= knight_zobrist_magic_number[1][C3];
+    expected_key ^= knight_zobrist_magic_number[1][D4];
+    // Bishops: E3, F1
+    expected_key ^= bishop_zobrist_magic_number[1][E3];
+    expected_key ^= bishop_zobrist_magic_number[1][F1];
+    // Queen: D2
+    expected_key ^= queen_zobrist_magic_number[1][D2];
+    // King: E1
+    expected_key ^= king_zobrist_magic_number[1][E1];
+    // Pawns: A2, B2, C2, E4, F3, G2, H2
+    expected_key ^= pawn_zobrist_magic_number[1][A2];
+    expected_key ^= pawn_zobrist_magic_number[1][B2];
+    expected_key ^= pawn_zobrist_magic_number[1][C2];
+    expected_key ^= pawn_zobrist_magic_number[1][E4];
+    expected_key ^= pawn_zobrist_magic_number[1][F3];
+    expected_key ^= pawn_zobrist_magic_number[1][G2];
+    expected_key ^= pawn_zobrist_magic_number[1][H2];
+
+    // Black Pieces
+    // Rooks: A8, F8
+    expected_key ^= rook_zobrist_magic_number[0][A8];
+    expected_key ^= rook_zobrist_magic_number[0][F8];
+    // Knights: C6, F6
+    expected_key ^= knight_zobrist_magic_number[0][C6];
+    expected_key ^= knight_zobrist_magic_number[0][F6];
+    // Bishops: C8, G7
+    expected_key ^= bishop_zobrist_magic_number[0][C8];
+    expected_key ^= bishop_zobrist_magic_number[0][G7];
+    // Queen: D8
+    expected_key ^= queen_zobrist_magic_number[0][D8];
+    // King: G8
+    expected_key ^= king_zobrist_magic_number[0][G8];
+    // Pawns: A7, B7, D6, E7, F7, G6, H7
+    expected_key ^= pawn_zobrist_magic_number[0][A7];
+    expected_key ^= pawn_zobrist_magic_number[0][B7];
+    expected_key ^= pawn_zobrist_magic_number[0][D6];
+    expected_key ^= pawn_zobrist_magic_number[0][E7];
+    expected_key ^= pawn_zobrist_magic_number[0][F7];
+    expected_key ^= pawn_zobrist_magic_number[0][G6];
+    expected_key ^= pawn_zobrist_magic_number[0][H7];
+
+    // White to move (no extra key)
+
+    // Castling Rights: KQ (8 + 4 = 12)
+    expected_key ^= castling_rights_zobrist_magic_number[12];
+
+    // En Passant: None (-1)
+
+    TEST_ASSERT_EQUAL_UINT64(expected_key, zobrist_key);
+}
