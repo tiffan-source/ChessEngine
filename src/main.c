2b86e19d76b9bc2c4e0a0ac47efaf32b7199cbc1
tiffan-source <zohoungbogboanlyou@gmail.com>
2026-01-08 14:40:31 +0100

    fix: corriger la configuration initiale et ajouter une limite de temps pour l'algorithme de recherche

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 int main(int argc, char *argv[]) {
     initialize_magic_zobrist_numbers();
     initialize_move_generation_databases();
-    init_config(9);
+    init_config(64);
 
     Game* internal_game = NULL;
     char command[COMMAND_MAX_LENGTH] = {0};
